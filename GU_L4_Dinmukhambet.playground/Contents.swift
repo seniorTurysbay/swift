96a3dfadf15012b7bc38a6df2c9bf2c3f0840eb3
Dinmukhambet <d.turysbay@life.kz>
2022-05-10 17:08:00 +0600

    practice

--- GU_L4_Dinmukhambet.playground/Contents.swift
+++ GU_L4_Dinmukhambet.playground/Contents.swift
@@ -1,5 +1,100 @@
 import Cocoa
 
+//enum HondaDoorState {
+//    case open, close
+//}
+////
+//enum UIColor {
+//    case white,black, blue
+//}
+////
+//enum Transmission {
+//    case manual, auto
+//}
+//struct Honda {
+//    let color: UIColor
+//    let mp3: Bool
+//    let transmission: Transmission
+//    var km: Double
+//    var doorState: HondaDoorState
+//    init(color: UIColor, mp3: Bool, transmission: Transmission, km: Double,
+//         doorState: HondaDoorState) {
+//        self.color = color
+//        self.mp3 = mp3
+//        self.transmission = transmission
+//        self.km = km
+//        self.doorState = doorState
+//    }
+//}
+//var car1 = Honda(color: .white, mp3: true, transmission: .auto, km: 0.0, doorState: .close)
+//var car2 = Honda(color: .white, mp3: false, transmission: .auto, km: 120.0, doorState: .close)
+////print(car1.km,", ", car2.km) //0.0, 120.0
+////car2 = car1
+////print(car1.km,", ", car2.km) //0.0, 0.0
+////car1.km = 500.0
+////print(car1.km,", ", car2.km) //500.0, 0.0
+//
+//print("car1",car1)
+//print("car2",car2)
+//car2 = car1
+//car1.km = 500
+//
+//print("car1",car1)
+//print("car2",car2)
+//class Honda {
+//    let color: UIColor
+//    let mp3: Bool
+//    let transmission: Transmission
+//    var km: Double
+//    var doorState: HondaDoorState
+//    // ключевое слово static указывает на то, что это свойство класса
+//    static var carCount = 0
+//    init(color: UIColor, mp3: Bool, transmission: Transmission, km: Double, doorState: HondaDoorState) {
+//        self.color = color
+//        self.mp3 = mp3
+//        self.transmission = transmission
+//        self.km = km
+//        self.doorState = doorState
+//        // в конструкторе будем увеличивать переменную на 1
+//        Honda.carCount += 1 }
+//    static func countInfo(){
+//        print("Выпущено \(self.carCount) автомобилей")
+//    }
+//
+//}
+//let car1 = Honda(color: .white, mp3: true, transmission: .auto, km: 0.0, doorState: .close)
+//let car2 = Honda(color: .white, mp3: true, transmission: .auto, km: 0.0, doorState: .close)
+//let car3 = Honda(color: .white, mp3: true, transmission: .auto, km: 0.0, doorState: .close)
+// обратимся к имени класса для доступа к свойству print(Honda.carCount) // 3
+//Honda.countInfo()
+// Выпущено 3 автомобилей
+//}
+//let car1 = Honda(color: .white, mp3: true, transmission: .auto, km: 0.0, doorState: .close)
+//let car2 = Honda(color: .white, mp3: true, transmission: .auto, km: 0.0, doorState: .close)
+//let car3 = Honda(color: .white, mp3: true, transmission: .auto, km: 0.0, doorState: .close)
+//обратимся к имени класса для доступа к свойству print(Honda.carCount) // 3
+//print(Honda.carCount)
+
+//class Rectangle {
+//    var sideA: Double // обязательные переменные
+//    var sideB: Double
+//    func getArea(){
+//        print(sideA*sideB)
+//    }
+//    init(sideA: Double, sideB: Double) { // обязательный конструктор
+//        self.sideA = sideA
+//        self.sideB = sideB
+//
+//    }
+//    convenience init(side: Double){ // вспомогательный конструктор для создания квадрата
+//        self.init(sideA: side, sideB: side)
+//    }
+//
+//}
+//let rect = Rectangle(side: 12)
+//rect.getArea()
+//print(rect)
+//
 enum UIColor {
     case white,black, blue,green
 }
